7cd3f352b780f4affd176d31c4c99690e511f2d9
pablolobat0 <plobatorey@gmail.com>
2025-03-10 18:16:50 +0100

    feat(backend): añadido hash de contraseña

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -2,14 +2,19 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
-    """Esquema para crear un usuario (sin ID)"""
     id: Optional[str]
     name: str
     surname: str
+    password: str
     email: EmailStr
     age: float
     height: float
     weight: float
     diabetes: bool
     hypertension: bool
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 
