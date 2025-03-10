9d8971026f83f50d2f4c05d890e658e84f94a157
Pablo Seijo <pgarsei510@gmail.com>
2025-03-10 18:46:32 +0100

    Init corregido porque soy medio parvo

--- frontend/app/(tabs)/plan.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function PlanScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-white text-xl font-bold">¡Tailwind está funcionando! 🎉</Text>
-    </View>
-  );
-}
