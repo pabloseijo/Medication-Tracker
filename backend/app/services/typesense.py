05f59f43757f61ed037441a707c62b453df677f0
ElRuflas <davidcarpidiaz@gmail.com>
2025-03-11 16:00:04 +0100

    Backend fixed, funciona pero devuelve respuesta vacia

--- backend/app/services/typesense.py
+++ backend/app/services/typesense.py
@@ -10,16 +10,42 @@ client = typesense.Client({
 })
 
 
-
-# Crear la colección de medicamentos en Typesense
+# Crear la colección de medicamentos en Typesense si no existe
 def create_meds_collection():
+    collection_name = "medications"
+
+    # Obtener la lista de colecciones existentes
+    existing_collections = [col["name"] for col in client.collections.retrieve()]
+
+    if collection_name in existing_collections:
+        print(f"La colección '{collection_name}' ya existe. No se creará nuevamente.")
+        return
+
     schema = {
-        "name": "medications",
+        "name": collection_name,
         "fields": [
             {"name": "name", "type": "string"},
         ]
     }
+
     client.collections.create(schema)
+    print(f"Creada la colección '{collection_name}' exitosamente.")
+    insert_meds()
+
+def insert_meds():
+    medications = [
+        {"name": "Paracetamol"},
+        {"name": "Ibuprofeno"},
+        {"name": "Amoxicilina"},
+        {"name": "Omeprazol"},
+        {"name": "Metformina"}
+    ]
+    
+    try:
+        client.collections["medications"].documents.import_(medications, {'action': 'upsert'})
+        print("Medicamentos insertados correctamente en la colección.")
+    except Exception as e:
+        print(f"Error al insertar medicamentos: {e}")
 
 
 # Función para buscar medicamentos con autocompletado
@@ -31,4 +57,4 @@ def search_medications(query: str):
     }
 
     result = client.collections['medications'].documents.search(search_parameters)
-    return result['hits']
+    return result
