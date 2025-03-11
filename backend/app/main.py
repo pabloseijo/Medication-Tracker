7ea5172a00ef76543a1ea7f37746178c44bf973f
pablolobat0 <plobatorey@gmail.com>
2025-03-11 11:51:07 +0100

    fix(backend): falta del uso del lifespan

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.database import  start_client, close_client
+from app.database import start_client, close_client
 from contextlib import asynccontextmanager
 from app.routes.meds import meds_router
+from app.routes.user import user_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -27,13 +29,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Medication Tracker API",
     description="API para gestionar el seguimiento de medicación",
-    version="0.0.1"
+    version="0.0.1",
+    lifespan=lifespan,
 )
 
-# Configuración de CORS 
+# Configuración de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -41,9 +44,10 @@ app.add_middleware(
 
 
 app.include_router(meds_router)
+app.include_router(user_router)
+
 
 # Endpoint raíz
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Bienvenido a la API de seguimiento de medicación"}
-
