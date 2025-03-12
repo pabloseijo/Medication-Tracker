abda784be635441e5814bebeb1b6e6b9bc94d1a2
David Carpintero Diaz <128366266+ElRuflas@users.noreply.github.com>
2025-03-12 18:12:55 +0100

    Endpoints de medicamentos creados (#46)

--- backend/app/crud/meds.py
+++ backend/app/crud/meds.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from motor.motor_asyncio import AsyncIOMotorCollection
 from app.schemas.meds import Medication
-
+from typing import List, Optional
 
 async def add_sporadic_medication(
     collection: AsyncIOMotorCollection, medication: Medication
@@ -21,3 +21,25 @@ async def add_treatment(
     result = await collection.insert_one(treatment.model_dump())
 
     return str(result.inserted_id)
+
+
+async def get_all_treatments(
+    collection: AsyncIOMotorCollection, user_id: str
+) -> List[dict]:
+    """Obtiene todos los tratamientos de un usuario."""
+    treatments = []
+    async for treatment in collection.find({"user_id": user_id}):
+        treatment["_id"] = str(treatment["_id"])  # Convertir ObjectId a string
+        treatments.append(treatment)
+    return treatments
+
+async def get_all_sporadic_medications(
+    collection: AsyncIOMotorCollection, user_id: str
+) -> List[dict]:
+    """Obtiene todas las medicaciones esporádicas de un usuario."""
+    medications = []
+    async for medication in collection.find({"user_id": user_id}):
+        medication["_id"] = str(medication["_id"])  # Convertir ObjectId a string
+        medication["user_id"] = str(medication["user_id"])  # Asegurarse de que user_id también sea str
+        medications.append(medication)
+    return medications
