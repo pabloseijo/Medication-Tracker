81d9964cb6db8d962de4ca7195be2f46a5c5abe8
ElRuflas <davidcarpidiaz@gmail.com>
2025-03-12 18:11:01 +0100

    Endpoints de medicamentos creados

--- backend/app/routes/meds.py
+++ backend/app/routes/meds.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Query, status
 from motor.motor_asyncio import AsyncIOMotorCollection
-from app.crud.meds import add_sporadic_medication, add_treatment
+from app.crud.meds import add_sporadic_medication, add_treatment, get_all_sporadic_medications, get_all_treatments
 from app.database import get_database
 from app.schemas.meds import Medication 
 from app.services.cima import (
@@ -9,7 +9,7 @@ from app.services.cima import (
     obtener_medicamentos_por_nombre,
 )
 from app.services.typesense import search_medications
-
+from fastapi.encoders import jsonable_encoder
 
 meds_router = APIRouter()
 
@@ -72,6 +72,54 @@ async def create_treatment(
             raise ValueError("No se ha podido insertar el tratamiento")
 
         return {"id": str(result)}
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
+
+
+@meds_router.get("/treatments")
+async def get_treatments(
+    db: AsyncIOMotorCollection = Depends(get_database),
+):
+    mock_user = await db.users.find_one({"email": "mock@example.com"})
+    if not mock_user:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    treatments = await db.treatments.find({"user_id": mock_user["_id"]}).to_list(None)
+    
+    # Convertir ObjectId a str en cada tratamiento
+    for treatment in treatments:
+        treatment["_id"] = str(treatment["_id"])
+        treatment["user_id"] = str(treatment["user_id"])
+
+    return treatments  
+
+@meds_router.get("/sporadic", status_code=status.HTTP_200_OK)
+async def read_sporadic_medications(
+    db: AsyncIOMotorCollection = Depends(get_database),
+):
+    """
+    Devuelve todas las medicaciones esporádicas del usuario mock.
+    Se busca el usuario mock y se filtran las medicaciones por user_id.
+    """
+    try:
+        # Buscar el usuario mock
+        mock_user = await db.users.find_one({"email": "mock@example.com"})
+        if not mock_user:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+        # Obtener las medicaciones esporádicas del usuario mock
+        medications = []
+        async for medication in db.sporadic_medication.find({"user_id": mock_user["_id"]}):
+            # Convertir ObjectId a str
+            medication["_id"] = str(medication["_id"])
+            medication["user_id"] = str(medication["user_id"])
+            medications.append(medication)
+
+        # Devolver las medicaciones
+        return medications
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
