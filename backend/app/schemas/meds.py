0957c4e335b4a5046b661300be58ac013eb9028d
pablolobat0 <plobatorey@gmail.com>
2025-03-12 11:23:36 +0100

    feat(backend): añadidos los modelos para los medicamentos del paciente

---
+++ backend/app/schemas/meds.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import date, time
+
+
+class SporadicMedicationCreate(BaseModel):
+    user_id: Optional[str]
+    name: str
+    date: date
+    time: time
+    dosage_amount: float
+    dosage_unit: str
+
+
+class TreatmentCreate(BaseModel):
+    user_id: Optional[str]
+    name: str
+    start_date: date
+    frequency_amount: int
+    frequency_unit: str
+    duration_days: int
+    dosage_amount: float
+    dosage_unit: str
