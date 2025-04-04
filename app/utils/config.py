9e16f959c1ea252323d5199d5e5cb9aa56cd3287
pablolobat0 <plobatorey@gmail.com>
2025-04-04 18:02:21 +0200

    fix: quick fix

--- app/utils/config.py
+++ app/utils/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 TYPESENSE_API_KEY = os.getenv("TYPESENSE_API_KEY")
 
 TYPESENSE_CONFIG = {
-    "host": "typesense",
-    "port": "8108",
-    "protocol": "http",
+    "host": "pu4o6da8m95f0ne7p-1.a1.typesense.net",
+    "port": "443",
+    "protocol": "https",
     "api_key": TYPESENSE_API_KEY,
 }
